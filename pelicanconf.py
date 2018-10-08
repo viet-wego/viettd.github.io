8f3b1141a58bf8fbe9fcac79b46027e340a43285
Viet Tran <tdviet12@gmail.com>
2018-10-08 23:48:47 +0800

    update theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'Viet Tran'
-SITENAME = "Get the shit done"
+SITENAME = 'Viet Tran'
 SITEURL = ''
 
 PATH = 'content'
@@ -27,11 +27,15 @@ AUTHOR_FEED_RSS = None
 
 # Social widget
 SOCIAL = (('GitHub', 'https://github.com/viettd'),
-          ('Twitter', 'https://twitter.com/tdvietus'))
+          ('Twitter', 'https://twitter.com/tdvietus'),
+          ('LinkedIn', 'https://www.linkedin.com/in/viettd/'))
 
 DEFAULT_PAGINATION = 11
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
 
-THEME = 'bootstrap'
+THEME = 'pelican-fresh'
+#SITETITLE = SITENAME
+#SITESUBTITLE = "I'm programmer, I still have life!"
+GRAVATAR_IMAGE = 'https://avatars2.githubusercontent.com/u/6848210?s=460&v=4'
